64135815a6ecce8fa6247eec0d5136c70e3e558c
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:35:54 +0530

    Create Reverse a String.java

---
+++ Recursion Problems/Reverse a String.java
@@ -0,0 +1,16 @@
+/*
+
+Example: Input: "hello" → Output: "olleh"
+
+*/
+
+class Main {
+    static String reverse(String s) {
+        if (s.isEmpty()) return s;
+        return reverse(s.substring(1)) + s.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("hello"));
+    }
+}
