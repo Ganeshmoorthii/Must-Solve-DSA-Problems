f18f137e62f6c8dea1683833ddaedde98a5e5c01
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:28:37 +0530

    Create Sum of First N Natural Numbers.java

---
+++ Pattern & Number logic problems/Sum of First N Natural Numbers.java
@@ -0,0 +1,14 @@
+/*
+
+Input: n = 10
+Output: 55
+
+*/
+
+class SumNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = n * (n + 1) / 2;
+        System.out.println("Sum = " + sum);
+    }
+}
