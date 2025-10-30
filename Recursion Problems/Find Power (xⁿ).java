98f8c953ba038533820b30677faa1b9b8735462d
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:38:18 +0530

    Create Find Power (xⁿ).java

---
+++ Recursion Problems/Find Power (xⁿ).java
@@ -0,0 +1,14 @@
+/*
+Example: Input: x = 2, n = 5 → Output: 32
+*/
+
+class Main {
+    static int power(int x, int n) {
+        if (n == 0) return 1;
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 5));
+    }
+}
