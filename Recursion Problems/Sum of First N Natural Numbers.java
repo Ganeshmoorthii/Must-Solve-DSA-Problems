bfb0dea68976938d478d8ad504afca19d08242f1
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:34:08 +0530

    Create Sum of First N Natural Numbers.java

---
+++ Recursion Problems/Sum of First N Natural Numbers.java
@@ -0,0 +1,16 @@
+/*
+
+Example: Input: 5 → Output: 15
+
+*/
+
+class Main {
+    static int sum(int n) {
+        if (n == 0) return 0;
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
