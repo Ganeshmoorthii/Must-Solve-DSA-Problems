aeb8411a33e4d00dbd1db54627cc05ffea9a1938
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 21:31:03 +0530

    Create Odd or Even Check.java

---
+++ Most commonly asked Problems/Odd or Even Check.java
@@ -0,0 +1,12 @@
+/*
+Example:
+Input → 7 → Odd
+Input → 10 → Even
+*/
+
+class OddEven {
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(n % 2 == 0 ? "Even" : "Odd");
+    }
+}
