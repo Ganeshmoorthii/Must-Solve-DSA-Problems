533c146e5d3dd5035728ccf298e48d23c3dd8ba3
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:39:34 +0530

    Create Find the Nth Fibonacci Number.java

---
+++ Recursion Problems/Find the Nth Fibonacci Number.java
@@ -0,0 +1,14 @@
+/*
+Example: Input: n = 7 → Output: 13
+*/
+
+class Main {
+    static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+}
