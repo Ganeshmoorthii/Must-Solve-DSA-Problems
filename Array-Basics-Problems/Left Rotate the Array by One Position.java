dbbbb25f8c89de4c5c340902960db341af04cf32
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 20:31:26 +0530

    Create Left Rotate the Array by One Position.java

---
+++ Array-Basics-Problems/Left Rotate the Array by One Position.java
@@ -0,0 +1,16 @@
+/*
+Example:
+Input → [1, 2, 3, 4, 5]
+Output → [2, 3, 4, 5, 1]
+*/
+
+import java.util.Arrays;
+class Q8_LeftRotate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int first = arr[0];
+        for (int i = 1; i < arr.length; i++) arr[i - 1] = arr[i];
+        arr[arr.length - 1] = first;
+        System.out.println(Arrays.toString(arr));
+    }
+}
