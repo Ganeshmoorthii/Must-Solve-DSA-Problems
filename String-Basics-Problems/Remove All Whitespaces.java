2f0851e73dbe7072a22b694797e5bbf6f7c1ee81
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 20:55:06 +0530

    Create Remove All Whitespaces.java

---
+++ String-Basics-Problems/Remove All Whitespaces.java
@@ -0,0 +1,15 @@
+/*
+Example:
+Input → "I am learning Java"
+Output → "IamlearningJava"
+*/
+
+class S5_RemoveWhitespaces {
+    public static void main(String[] args) {
+        String s = "I am learning Java";
+        String res = s.replaceAll("\\s+", "");
+        System.out.println("Without spaces: " + res);
+    }
+}
+
+// Time: O(n)
