d05eb53632ae62d5a4a186663c74c23becb6a9d7
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:25:37 +0530

    Create Multiplication Table of a Number.java

---
+++ Pattern & Number logic problems/Multiplication Table of a Number.java
@@ -0,0 +1,18 @@
+/*
+
+Input: n = 5
+Output:
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+
+*/
+
+class MultiplicationTable {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 10; i++)
+            System.out.println(n + " x " + i + " = " + (n * i));
+    }
+}
