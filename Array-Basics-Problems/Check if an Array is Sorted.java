6e793f97de2866ce41bc0361301f5d9c35525634
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 20:09:26 +0530

    Create Check if an Array is Sorted.java

---
+++ Array-Basics-Problems/Check if an Array is Sorted.java
@@ -0,0 +1,26 @@
+/*
+Example:
+Input → [1, 2, 3, 4] → Ascending 
+Input → [9, 7, 5] → Descending 
+*/
+
+class Q3_CheckSorted {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {9, 7, 5};
+        System.out.println(isSortedAscending(arr1)); // true
+        System.out.println(isSortedDescending(arr2)); // true
+    }
+
+    static boolean isSortedAscending(int[] arr) {
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] < arr[i - 1]) return false;
+        return true;
+    }
+
+    static boolean isSortedDescending(int[] arr) {
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] > arr[i - 1]) return false;
+        return true;
+    }
+}
