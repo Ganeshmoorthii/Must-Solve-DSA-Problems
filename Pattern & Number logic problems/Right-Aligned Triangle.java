0827312a5472cc69837ef1f91e2a9abb8adc560a
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 22:22:20 +0530

    Create Right-Aligned Triangle.java

---
+++ Pattern & Number logic problems/Right-Aligned Triangle.java
@@ -0,0 +1,24 @@
+/*
+
+Input: n = 5
+Output:
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+class RightAligned {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int space = n - i; space > 0; space--)
+                System.out.print(" ");
+            for (int star = 1; star <= i; star++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
