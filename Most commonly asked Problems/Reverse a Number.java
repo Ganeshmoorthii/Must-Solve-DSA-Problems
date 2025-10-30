f858bc08e394e594816a74fc4fc5c53d21904002
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-10-30 21:29:15 +0530

    Create Reverse a Number.java

---
+++ Most commonly asked Problems/Reverse a Number.java
@@ -0,0 +1,18 @@
+/*
+Example:
+Input → 1234
+Output → 4321
+*/
+
+class ReverseNumber {
+    public static void main(String[] args) {
+        int n = 1234, rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Reversed = " + rev);
+    }
+}
